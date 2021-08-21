c352410aa6874f28bc313fe9d14433b7356f1d88
Canh Huy <huycanh14@gmail.con>
2021-08-21 22:33:12 +0700

    useState React Hook

--- react-movies/src/Events.tsx
+++ react-movies/src/Events.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+
 export default function Events() {
-    let canSee = true;
+    // let canSee = true;
+    const [canSee, setCanSee] = useState(false);
+    const [text, setText] = useState('');
 
     function handCheckboxChanged() {
         // alert("My value was changed");    
-        canSee = !canSee;
+        // canSee = !canSee;
+        setCanSee(!canSee);
     }
 
     function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
         console.log(e.currentTarget.value);
+        setText(e.currentTarget.value);
     }
 
     return (
@@ -23,6 +29,9 @@ export default function Events() {
                 <input type="text" onKeyUp={(e) => handleKeyUp(e)} />
             </div>
             {canSee ? <div>You are seeing the secret</div> : <span>You are not allowed to see</span>}
+            <div>
+                {text}
+            </div>
         </>
     )
 }
