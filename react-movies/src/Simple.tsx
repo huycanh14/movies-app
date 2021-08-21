f37d8e8fd4611724dfbf32ef4b0941373670d11b
Canh Huy <huycanh14@gmail.con>
2021-08-21 08:21:13 +0700

    note

--- react-movies/src/Simple.tsx
+++ react-movies/src/Simple.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Simple() {
     return (
         <React.Fragment>
+            {/* Fragment thay thế thẻ bao ngoài. Ko làm ảnh đến component cha */}
             <h1>Hello World!</h1>
             <h2>This is a subtext</h2>
         </React.Fragment>
