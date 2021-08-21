20e45e1e240f87dd2f86bcfea93c83f3d04537c2
Canh Huy <huycanh14@gmail.con>
2021-08-21 23:41:09 +0700

    truyền tham số tư cha qua con

---
+++ react-movies/src/ConditionalsIfSeveralComponent.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Simple from "./Simple";
+import SelectNumber from "./SelectNumber";
+
+export default function ConditionalsIfSeveralComponent() {
+    const [selectedRate, setSelectedRate] = useState(1);
+
+    function displayResult() {
+        if (selectedRate === 1)
+            return <span>Noooo!</span>
+        else if (selectedRate === 2)
+            return (
+                <>
+                    <input type="text" />
+                </>
+            );
+        else if (selectedRate === 3)
+            <><Simple /></>
+        else
+             return <span>Thanks!</span>
+    }
+    return (
+        <>
+            <h1>Conditionals If example</h1>
+            <div>Rate this website</div>
+            <SelectNumber maxValue={10}/>
+            <div>
+                {displayResult()}
+            </div>
+        </>
+    )
+}
---
+++ react-movies/src/SelectNumber.tsx
@@ -0,0 +1,17 @@
+export default function SelectNumber(props: selectNumberProps) {
+    const arr = Array(props.maxValue).fill(0);
+    return (
+        <>
+            <select name="" id="" onChange={(e) => {
+                // setSelectedRate(parseInt(e.target.value, 10));
+            }}>
+                {arr.map((_, index) => <option key={index+1} value={index+1}>{index+1}</option>)}
+                {/* arr.map((_, index) : '_': ko muốn nói gì, ko nói đén giá trị */}
+            </select>
+        </>
+    );
+}
+
+interface selectNumberProps {
+    maxValue: number;
+}
