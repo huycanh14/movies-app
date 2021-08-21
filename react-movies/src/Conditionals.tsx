d7e0618c40530da8bbc27794ba2537f96ecd3615
Canh Huy <huycanh14@gmail.con>
2021-08-21 09:04:13 +0700

    Conditionals

---
+++ react-movies/src/Conditionals.tsx
@@ -0,0 +1,7 @@
+export default function Conditionals() {
+    const canSee = true;
+    return (<>
+        <h1>Conditionals Example</h1>
+        {canSee ? <div>You are seeing the secret</div> : <span>You are not allowed to see</span>}
+    </>)
+}
